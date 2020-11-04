e12a5f160427847c39c3ba981c66aaf5b4471b61
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-11-03 20:39:58 -0500

    Calcualte max size of message and update GUI

--- Stag/Main_Page.Designer.cs
+++ Stag/Main_Page.Designer.cs
@@ -30,19 +30,48 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.Button btn_openFile;
+            System.Windows.Forms.Button Save;
             this.process1 = new System.Diagnostics.Process();
             this.txtBx_filePath = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic_image = new System.Windows.Forms.PictureBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnl_msg = new System.Windows.Forms.Panel();
+            this.lbl_currChars = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_maxMsgSizeVal = new System.Windows.Forms.Label();
+            this.lbl_maxMsgSize = new System.Windows.Forms.Label();
+            this.lbl_msgEnc = new System.Windows.Forms.Label();
+            this.rTxt_imageMsg = new System.Windows.Forms.RichTextBox();
             btn_openFile = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_image)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            this.pnl_msg.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btn_openFile
+            // 
+            btn_openFile.Location = new System.Drawing.Point(525, 376);
+            btn_openFile.Name = "btn_openFile";
+            btn_openFile.Size = new System.Drawing.Size(86, 29);
+            btn_openFile.TabIndex = 1;
+            btn_openFile.Text = "Open";
+            btn_openFile.UseVisualStyleBackColor = true;
+            btn_openFile.Click += new System.EventHandler(this.btn_openFile_Click);
+            // 
+            // Save
+            // 
+            Save.Location = new System.Drawing.Point(546, 51);
+            Save.Name = "Save";
+            Save.Size = new System.Drawing.Size(86, 29);
+            Save.TabIndex = 11;
+            Save.Text = "Save";
+            Save.UseVisualStyleBackColor = true;
             // 
             // process1
             // 
@@ -58,26 +87,18 @@
             // 
             this.txtBx_filePath.Location = new System.Drawing.Point(39, 379);
             this.txtBx_filePath.Name = "txtBx_filePath";
+            this.txtBx_filePath.ReadOnly = true;
             this.txtBx_filePath.Size = new System.Drawing.Size(480, 22);
             this.txtBx_filePath.TabIndex = 0;
+            this.txtBx_filePath.Text = "File Path";
             // 
-            // btn_openFile
+            // pic_image
             // 
-            btn_openFile.Location = new System.Drawing.Point(525, 376);
-            btn_openFile.Name = "btn_openFile";
-            btn_openFile.Size = new System.Drawing.Size(86, 29);
-            btn_openFile.TabIndex = 1;
-            btn_openFile.Text = "Open";
-            btn_openFile.UseVisualStyleBackColor = true;
-            btn_openFile.Click += new System.EventHandler(this.btn_openFile_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(53, 80);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(569, 275);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pic_image.Location = new System.Drawing.Point(53, 80);
+            this.pic_image.Name = "pic_image";
+            this.pic_image.Size = new System.Drawing.Size(569, 275);
+            this.pic_image.TabIndex = 2;
+            this.pic_image.TabStop = false;
             // 
             // contextMenuStrip1
             // 
@@ -93,7 +114,7 @@
             this.optionsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(657, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(720, 28);
             this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -109,13 +130,13 @@
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
             this.saveToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem1
             // 
             this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
-            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(128, 26);
             this.saveToolStripMenuItem1.Text = "Save";
             // 
             // optionsToolStripMenuItem
@@ -124,21 +145,93 @@
             this.optionsToolStripMenuItem.Size = new System.Drawing.Size(75, 24);
             this.optionsToolStripMenuItem.Text = "Options";
             // 
+            // pnl_msg
+            // 
+            this.pnl_msg.Controls.Add(this.lbl_currChars);
+            this.pnl_msg.Controls.Add(this.label1);
+            this.pnl_msg.Controls.Add(this.lbl_maxMsgSizeVal);
+            this.pnl_msg.Controls.Add(this.lbl_maxMsgSize);
+            this.pnl_msg.Controls.Add(this.lbl_msgEnc);
+            this.pnl_msg.Controls.Add(Save);
+            this.pnl_msg.Controls.Add(this.rTxt_imageMsg);
+            this.pnl_msg.Location = new System.Drawing.Point(30, 411);
+            this.pnl_msg.Name = "pnl_msg";
+            this.pnl_msg.Size = new System.Drawing.Size(678, 196);
+            this.pnl_msg.TabIndex = 5;
+            this.pnl_msg.Visible = false;
+            // 
+            // lbl_currChars
+            // 
+            this.lbl_currChars.AutoSize = true;
+            this.lbl_currChars.Location = new System.Drawing.Point(614, 167);
+            this.lbl_currChars.Name = "lbl_currChars";
+            this.lbl_currChars.Size = new System.Drawing.Size(16, 17);
+            this.lbl_currChars.TabIndex = 16;
+            this.lbl_currChars.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(547, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "# Chars:";
+            // 
+            // lbl_maxMsgSizeVal
+            // 
+            this.lbl_maxMsgSizeVal.AutoSize = true;
+            this.lbl_maxMsgSizeVal.Location = new System.Drawing.Point(168, 13);
+            this.lbl_maxMsgSizeVal.Name = "lbl_maxMsgSizeVal";
+            this.lbl_maxMsgSizeVal.Size = new System.Drawing.Size(46, 17);
+            this.lbl_maxMsgSizeVal.TabIndex = 14;
+            this.lbl_maxMsgSizeVal.Text = "label1";
+            // 
+            // lbl_maxMsgSize
+            // 
+            this.lbl_maxMsgSize.AutoSize = true;
+            this.lbl_maxMsgSize.Location = new System.Drawing.Point(-1, 13);
+            this.lbl_maxMsgSize.Name = "lbl_maxMsgSize";
+            this.lbl_maxMsgSize.Size = new System.Drawing.Size(162, 17);
+            this.lbl_maxMsgSize.TabIndex = 13;
+            this.lbl_maxMsgSize.Text = "Maximum Message Size:";
+            // 
+            // lbl_msgEnc
+            // 
+            this.lbl_msgEnc.AutoSize = true;
+            this.lbl_msgEnc.Location = new System.Drawing.Point(-1, 30);
+            this.lbl_msgEnc.Name = "lbl_msgEnc";
+            this.lbl_msgEnc.Size = new System.Drawing.Size(178, 17);
+            this.lbl_msgEnc.TabIndex = 12;
+            this.lbl_msgEnc.Text = "Message Ecoded In Image:";
+            // 
+            // rTxt_imageMsg
+            // 
+            this.rTxt_imageMsg.Location = new System.Drawing.Point(2, 51);
+            this.rTxt_imageMsg.Name = "rTxt_imageMsg";
+            this.rTxt_imageMsg.Size = new System.Drawing.Size(538, 133);
+            this.rTxt_imageMsg.TabIndex = 10;
+            this.rTxt_imageMsg.Text = "";
+            this.rTxt_imageMsg.TextChanged += new System.EventHandler(this.rTxt_imageMsg_TextChanged);
+            // 
             // Main_Page
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(657, 451);
+            this.ClientSize = new System.Drawing.Size(720, 625);
+            this.Controls.Add(this.pnl_msg);
             this.Controls.Add(this.menuStrip1);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pic_image);
             this.Controls.Add(btn_openFile);
             this.Controls.Add(this.txtBx_filePath);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Main_Page";
             this.Text = "Image Steganography";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_image)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.pnl_msg.ResumeLayout(false);
+            this.pnl_msg.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -152,9 +245,16 @@
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_image;
         private System.Windows.Forms.TextBox txtBx_filePath;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel pnl_msg;
+        private System.Windows.Forms.Label lbl_maxMsgSizeVal;
+        private System.Windows.Forms.Label lbl_maxMsgSize;
+        private System.Windows.Forms.Label lbl_msgEnc;
+        private System.Windows.Forms.RichTextBox rTxt_imageMsg;
+        private System.Windows.Forms.Label lbl_currChars;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Stag/Main_Page.cs
+++ Stag/Main_Page.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,13 @@ namespace Stag
 {
     public partial class Main_Page : Form
     {
+        private int numBitsUse = 1; 
+        private Bitmap bitMap; 
+        
         public Main_Page()
         {
             InitializeComponent();
+            pic_image.SizeMode = PictureBoxSizeMode.StretchImage; 
         }
 
         private void btn_openFile_Click(object sender, EventArgs e)
@@ -22,19 +28,58 @@ namespace Stag
             //open a file for import
             var fileContent = string.Empty;
             var filePath = string.Empty; 
+            const string msgStart = "!*"; 
+            const string msgEnd = "*!"; 
+
             using (OpenFileDialog fileDialog = new OpenFileDialog())
             {
                 fileDialog.InitialDirectory = "c:\\";
-                fileDialog.Filter = "Image Files (*.jpg *.png)|*.jpg *.png| All files (*.*)|*.*";
+                fileDialog.Filter = "Image Files (*.jpg *.png)|*.jpg | All files (*.*)|*.*";
                 fileDialog.FilterIndex = 2;
                 fileDialog.RestoreDirectory = true; 
 
                 if (fileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    filePath = fileDialog.FileName;
-                    txtBx_filePath.Text = filePath;
+                    try
+                    {
+                        filePath = fileDialog.FileName;
+                        txtBx_filePath.Text = filePath;
+                        using (var bmpStream = System.IO.File.Open(filePath, System.IO.FileMode.Open))
+                        {
+                            Image image = Image.FromStream(bmpStream);
+                            bitMap = new Bitmap(image);
+                            pic_image.Image = image;
+                        }
+                        pnl_msg.Visible = true; 
+                    }catch (Exception ex)
+                    {
+                        MessageBox msgBox;
+                        string message = "An error occurred while opening the file: " + ex.Message;
+                        MessageBox.Show(message); 
+                    }
+
+                    lbl_maxMsgSizeVal.Text = calculateMaxMsgSize(bitMap).ToString(); 
                 }
             }
+        }
+
+        private long calculateMaxMsgSize(Bitmap bitMap)
+        {
+            int totalNumPixels = bitMap.Width * bitMap.Height;
+            int numPixelsNeeded = 8 / numBitsUse;
+            int msgSize = totalNumPixels / numPixelsNeeded; 
+            return msgSize - 4; //subtract 4 in order to allow for start and end of message flags  
+        }
+
+        private byte convertCharToByte(char target)
+        {
+            byte charByte = Convert.ToByte(target);
+            return charByte; 
+        }
+
+        private void rTxt_imageMsg_TextChanged(object sender, EventArgs e)
+        {
+            lbl_currChars.Text = rTxt_imageMsg.Text.Length.ToString(); 
         }
     }
 }
